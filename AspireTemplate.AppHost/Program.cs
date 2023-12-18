3561db5accaec7f5be645e41387ccfae0f4936b4
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2023-12-18 23:32:26 +0700

    cleanup

--- AspireTemplate.AppHost/Program.cs
+++ AspireTemplate.AppHost/Program.cs
@@ -2,7 +2,12 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var apiservice = builder.AddProject<Projects.AspireTemplate_ApiService>("apiservice");
 
+var messaging = builder.AddRabbitMQContainer("messaging", password: "saeed");
+
 builder.AddProject<Projects.AspireTemplate_Web>("webfrontend")
     .WithReference(apiservice);
+
+builder.AddProject<Projects.AspireTemplate_RabbitMQ>("consumers")
+    .WithReference(messaging);
 
 builder.Build().Run();
