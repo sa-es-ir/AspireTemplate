9f70186162340df71dd6dc7785ab1542fb92dcc1
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2023-12-23 12:14:19 +0700

    add consumer project

---
+++ AspireTemplate.RabbitMQConsumers/OrderProcessingJob.cs
@@ -0,0 +1,67 @@
+﻿
+using System.Diagnostics;
+using System.Text.Json;
+using AspireTemplate.Common;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace AspireTemplate.RabbitMQConsumers;
+
+public class OrderProcessingJob : BackgroundService
+{
+    private readonly ILogger<OrderProcessingJob> _logger;
+    private readonly IConfiguration _config;
+    private readonly IServiceProvider _serviceProvider;
+    private IConnection? _messageConnection;
+    private IModel? _messageChannel;
+
+    public OrderProcessingJob(ILogger<OrderProcessingJob> logger, IConfiguration config, IServiceProvider serviceProvider)
+    {
+        _logger = logger;
+        _config = config;
+        _serviceProvider = serviceProvider;
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        const string configKeyName = "RabbitMQ:QueueName";
+        string queueName = _config[configKeyName] ?? "orders";
+
+        _messageConnection = _serviceProvider.GetService<IConnection>();
+
+        _messageChannel = _messageConnection!.CreateModel();
+        _messageChannel.QueueDeclare(queueName, exclusive: false);
+
+        var consumer = new EventingBasicConsumer(_messageChannel);
+        consumer.Received += ProcessMessageAsync;
+
+        _messageChannel.BasicConsume(queue: queueName,
+                                     autoAck: true,
+                                     consumer: consumer);
+
+        return Task.CompletedTask;
+    }
+
+    public override async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await base.StopAsync(cancellationToken);
+
+        _messageChannel?.Dispose();
+    }
+
+    private void ProcessMessageAsync(object? sender, BasicDeliverEventArgs args)
+    {
+        _logger.LogInformation($"Processing Order at: {DateTime.UtcNow} with messageId: {args.BasicProperties.MessageId}");
+
+        var message = args.Body;
+
+        var model = JsonSerializer.Deserialize<OrderModel>(message.Span);
+
+        _logger.LogInformation($"Message Name: {model!.Name} and Amount: {model!.Amount}");
+
+    }
+}
---
+++ AspireTemplate.RabbitMQConsumers/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using AspireTemplate.RabbitMQConsumers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.AddServiceDefaults();
+
+builder.AddRabbitMQ("messaging");
+
+builder.Services.AddHostedService<OrderProcessingJob>();
+
+var host = builder.Build();
+host.Run();
