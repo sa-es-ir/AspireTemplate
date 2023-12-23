2d6b3c16d5226d7eb15960816dfa6175926ca789
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2023-12-23 12:14:48 +0700

    setup app host to run consumers

--- AspireTemplate.AppHost/Program.cs
+++ AspireTemplate.AppHost/Program.cs
@@ -1,8 +1,15 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiservice = builder.AddProject<Projects.AspireTemplate_ApiService>("apiservice");
+var rabbit = builder.AddRabbitMQContainer("messaging", password: "aspire");
+
+var apiservice = builder.AddProject<Projects.AspireTemplate_ApiService>("apiservice")
+    .WithReference(rabbit);
+
 
 builder.AddProject<Projects.AspireTemplate_Web>("webfrontend")
     .WithReference(apiservice);
+
+builder.AddProject<Projects.AspireTemplate_RabbitMQConsumers>("consumers")
+    .WithReference(rabbit);
 
 builder.Build().Run();
