8008e57c1294503ffd1a0d5743cf6e871b2805b4
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2023-12-23 12:15:06 +0700

    add api to publish a message

--- AspireTemplate.ApiService/Program.cs
+++ AspireTemplate.ApiService/Program.cs
@@ -1,9 +1,13 @@
+using AspireTemplate.Common;
+using RabbitMQ.Client;
 using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add service defaults & Aspire components.
 builder.AddServiceDefaults();
+
+builder.AddRabbitMQ("messaging");
 
 // Add services to the container.
 builder.Services.AddProblemDetails();
@@ -31,6 +35,27 @@ app.MapGet("/weatherforecast", () =>
     return forecast;
 });
 
+
+app.MapGet("/send-message", (IConnection messageConnection, IConfiguration configuration) =>
+{
+    const string configKeyName = "RabbitMQ:QueueName";
+    string? queueName = configuration[configKeyName];
+
+    using var channel = messageConnection.CreateModel();
+    channel.QueueDeclare(queueName, exclusive: false);
+    channel.BasicPublish(
+        exchange: "",
+        routingKey: queueName,
+        basicProperties: null,
+        body: JsonSerializer.SerializeToUtf8Bytes(new OrderModel
+        {
+            Name = $"Message from API: {Guid.NewGuid()}",
+            Amount = 1000
+        }));
+
+    return Results.Ok("message sent");
+});
+
 app.MapDefaultEndpoints();
 
 app.Run();
@@ -39,3 +64,4 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+
