3f4fe55a9f783a6b8fd503435560b132c83d76e4
Saeed Esmaeelinejad <saeed.esmaeelinejad@7peakssoftware.com>
2024-01-11 22:13:27 +0700

    Add aspire component path

---
+++ AspireTemplate.Components/RabbitMQConsumers/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RabbitMQConsumers;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.AddServiceDefaults();
+
+builder.AddRabbitMQ("messaging");
+
+builder.Services.AddHostedService<OrderProcessingJob>();
+
+var host = builder.Build();
+host.Run();
