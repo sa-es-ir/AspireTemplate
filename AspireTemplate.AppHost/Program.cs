d43e0465b8818346cfd35a50917f8543363d4d65
sa-es-ir <s.esmaeelinejad@gmail.com>
2024-01-11 22:25:55 +0700

    add Redis to apphost

--- AspireTemplate.AppHost/Program.cs
+++ AspireTemplate.AppHost/Program.cs
@@ -2,8 +2,11 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var rabbit = builder.AddRabbitMQContainer("messaging", password: "aspire");
 
+var redis = builder.AddRedisContainer("rediscache");
+
 var apiservice = builder.AddProject<Projects.AspireTemplate_ApiService>("apiservice")
-    .WithReference(rabbit);
+    .WithReference(rabbit)
+    .WithReference(redis);
 
 
 builder.AddProject<Projects.AspireTemplate_Web>("webfrontend")
